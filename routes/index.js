d8596fb2a401ffd67c6bbe4e16cc5d77334e4be2
jirevwe <rtukpe@gmail.com>
2017-03-06 10:08:49 +0100

    fixed basic routing problems

--- routes/index.js
+++ routes/index.js
@@ -4,43 +4,43 @@ var passport = require('passport');
 var User = require('../models/user');
 
 router.get('/', function(req, res, next) {
-  console.log(req.user);
-  res.render('index', { user : req.user });
+	res.render('index', { user : req.user });
 });
 
 router.get('/ping', function(req, res){
-  res.send('pong!', 200);
+	res.send('pong!');
 });
 
 router.get('/register', function(req, res, next) {
-  res.render('register', { });
+	if(req.isAuthenticated()){
+		res.redirect('/')
+	}
+	res.render('register');
 });
 
 router.post('/register', function(req, res) {
-  User.register(new User({ username : req.body.username }), req.body.password, function(err, account) {
-     if (err) {
-          return res.render("register", {info: "Sorry. That username already exists. Try again."});
-      }
-
-      passport.authenticate('local')(req, res, function () {
-        console.log(req.user);
-        res.redirect('/');
-      });
-  });
+	User.register(new User({ username : req.body.username }), req.body.password, function(err, account) {
+		 if (err) {
+					return res.render("register", {info: "Sorry. That username already exists. Try again."});
+			}
+			passport.authenticate('local')(req, res, function () {
+				res.redirect('/');
+			});
+	});
 });
 
 router.get('/login', function(req, res) {
-    res.render('login', { user : req.user });
+	if(req.isAuthenticated()){
+		res.redirect('/')
+	}
+	res.render('login');
 });
 
-router.post('/login', passport.authenticate('local', { 
-  successRedirect: "/",
-    failureRedirect: "/login"
- }));
+router.post('/login', passport.authenticate('local', { successRedirect: "/", failureRedirect: "/login" }));
 
 router.get('/logout', function(req, res) {
-    req.logout();
-    res.redirect('/');
+		req.logout();
+		res.redirect('/');
 });
 
 module.exports = router;
